bf217ef2f3a127e1a5d9df568dcf325bc0de7695
rafaelcarbono <rafael.carbono@outlook.com>
2022-03-28 10:19:06 -0300

    Adicionando níveis de edição e adição de posts, teams e imagens

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -2,19 +2,31 @@ import {Request, Response} from 'express'
 
 export const inicial = (req:Request, res: Response) =>{
 
-    res.render("pages/inicial",{
+    res.render("pages/inicial")
+}
 
+export const teams = (req:Request, res: Response) =>{
+    let slug:string = req.params.slug
 
-    })
+    res.render(`pages/teams/${slug}`)
+}
+
+export const galery = (req:Request, res: Response) =>{
+    let slug:string = req.params.slug
+
+    res.render(`pages/galery/${slug}`)
+}
+
+export const portal = (req:Request, res: Response) =>{
+    let slug:string = req.params.slug
+
+    res.render(`pages/portal/${slug}`)
 }
 
 export const slug = ((req:Request, res:Response) => {
 
-    let sluger:string = req.params.slug
+    let slug:string = req.params.slug
 
-    res.render(`pages/${sluger}`)
-
-    
+    res.render(`pages/${slug}`)
 
 })
-
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,9 @@ import * as homeController from '../controllers/homeController'
 const router = Router()
 router.get('/',homeController.inicial);
 router.get('/:slug',homeController.slug)
-
+router.get('/teams/:slug',homeController.teams)
+router.get('/portal/:slug',homeController.portal)
+router.get('/galery/:slug',homeController.galery)
 
 
 export default router
